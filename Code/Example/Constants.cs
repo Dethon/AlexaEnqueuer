0947cd959f7a47676b69b76ee34415e204f6aa02
Francisco Crespo <fran_cg@outlook.com>
2020-11-16 11:50:57 +0100

    Changed constants

---
+++ Code/Example/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace AlexaEnqueuerExample {
+    public static class Skill {
+        public const string computerControl = "computerControl";
+    }
+
+    public static class Intent {
+        // You are forced to implement these intents for all Amazon Alexa skills
+        public const string cancel = "AMAZON.CancelIntent";
+        public const string stop = "AMAZON.StopIntent";
+        public const string navigateHome = "AMAZON.NavigateHomeIntent";
+        public const string help = "AMAZON.HelpIntent";
+
+        //These are custom intents
+        public const string computerOn = "computerOn";
+        public const string computerOff = "computerOff";
+        public const string prepareForWork = "prepareForWork";
+    }
+}
--- Code/Utils/Constants.cs
+++ Code/Utils/Constants.cs
@@ -5,28 +5,9 @@
     }
 
     public static class VariableName {
-        // Use your own setting names
         public const string queue = "QueueName";
         public const string serviceBusUri = "ServiceBusConnection";
         public const string allowedUserId = "AllowedUserId";
         public const string userRestriction = "UserRestriction";
-    }
-
-    public static class Skill {
-        // Customize this to your heart's content
-        public const string computerControl = "computerControl";
-    }
-
-    public static class Intent {
-        // You are forced to implement these intents for all Amazon Alexa skills
-        public const string cancel = "AMAZON.CancelIntent";
-        public const string stop = "AMAZON.StopIntent";
-        public const string navigateHome = "AMAZON.NavigateHomeIntent";
-        public const string help = "AMAZON.HelpIntent";
-
-        // Customize this to your heart's content
-        public const string computerOn = "computerOn";
-        public const string computerOff = "computerOff";
-        public const string prepareForWork = "prepareForWork";
     }
 }
